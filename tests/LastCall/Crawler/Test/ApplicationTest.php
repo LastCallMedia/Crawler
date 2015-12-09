a7216a8a05650864d0ee21cd3c1baf373771d34b
Rob Bayliss <rob@lastcallmedia.com>
2015-12-09 08:47:04 -0500

    Add application tests

---
+++ tests/LastCall/Crawler/Test/ApplicationTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rfbayliss
+ * Date: 12/9/15
+ * Time: 8:44 AM
+ */
+
+namespace LastCall\Crawler\Test;
+
+
+use LastCall\Crawler\Application;
+
+class ApplicationTest extends \PHPUnit_Framework_TestCase {
+
+    public function testDefaultCommands() {
+        $application = new Application();
+        $this->assertInstanceOf('LastCall\Crawler\Command\ClearCommand', $application->get('clear'));
+        $this->assertInstanceOf('LastCall\Crawler\Command\SetupCommand', $application->get('setup'));
+        $this->assertInstanceOf('LastCall\Crawler\Command\TeardownCommand', $application->get('teardown'));
+        $this->assertInstanceOf('LastCall\Crawler\Command\CrawlCommand', $application->get('crawl'));
+    }
+
+    public function testDefaultHelpers() {
+        $application = new Application();
+        $this->assertInstanceOf('LastCall\Crawler\Helper\CrawlerHelper', $application->getHelperSet()->get('crawler'));
+        $this->assertInstanceOf('LastCall\Crawler\Helper\ProfilerHelper', $application->getHelperSet()->get('profiler'));
+    }
+}
