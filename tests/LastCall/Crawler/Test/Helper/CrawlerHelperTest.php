28b6bf5ebb887942d011e6be9b953abf3b9829ba
Rob Bayliss <rob@lastcallmedia.com>
2015-12-09 11:22:00 -0500

    Add test for getting a profiling crawler

--- tests/LastCall/Crawler/Test/Helper/CrawlerHelperTest.php
+++ tests/LastCall/Crawler/Test/Helper/CrawlerHelperTest.php
@@ -5,7 +5,14 @@ namespace LastCall\Crawler\Test\Helper;
 use LastCall\Crawler\Configuration\Configuration;
 use LastCall\Crawler\Helper\CrawlerHelper;
 use LastCall\Crawler\Configuration\ConfigurationInterface;
+use LastCall\Crawler\Helper\ProfilerHelper;
+use Prophecy\Argument;
+use Symfony\Component\Console\Helper\HelperSet;
 use Symfony\Component\Console\Output\NullOutput;
+use Symfony\Component\EventDispatcher\Debug\TraceableEventDispatcher;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Stopwatch\Stopwatch;
 
 class CrawlerHelperTest extends \PHPUnit_Framework_TestCase {
 
@@ -39,8 +46,6 @@ class CrawlerHelperTest extends \PHPUnit_Framework_TestCase {
         $helper->getConfiguration($file, new NullOutput());
     }
 
-
-
     public function testGetConfiguration() {
         $file = $this->writeTempConfig(sprintf("<?php\nreturn new %s('http://google.com');", Configuration::class));
         $helper = new CrawlerHelper();
@@ -48,6 +53,21 @@ class CrawlerHelperTest extends \PHPUnit_Framework_TestCase {
         $config = $helper->getConfiguration($file, $output);
         $this->assertInstanceOf(ConfigurationInterface::class, $config);
         $this->assertEquals('http://google.com', $config->getBaseUrl());
+    }
+
+    public function testGetProfilingCrawler() {
+        $helper = new CrawlerHelper();
+        $profilerHelper = $this->prophesize(ProfilerHelper::class);
+        $profilerHelper->getName()->willReturn('profiler');
+        $profilerHelper
+            ->getTraceableDispatcher(Argument::type(EventDispatcherInterface::class))
+            ->willReturn(new TraceableEventDispatcher(new EventDispatcher(), new Stopwatch()))
+            ->shouldBeCalled();
+        $profilerHelper->setHelperSet(Argument::any())->willReturn(NULL);
+
+        $set = new HelperSet([$helper, $profilerHelper->reveal()]);
+        $config = new Configuration('https://lastcallmedia.com');
+        $helper->getCrawler($config, TRUE);
     }
 
     public function testGetCrawler() {
