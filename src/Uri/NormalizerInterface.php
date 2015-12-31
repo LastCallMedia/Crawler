197ba1e70e73dfdc06a40acefeeffbe079eb482c
Rob Bayliss <rob@lastcallmedia.com>
2015-12-31 16:55:24 -0500

    Add doc for NormalizerInterface

--- src/Uri/NormalizerInterface.php
+++ src/Uri/NormalizerInterface.php
@@ -4,14 +4,17 @@ namespace LastCall\Crawler\Uri;
 
 use Psr\Http\Message\UriInterface;
 
+/**
+ * Modifies a URI so it follows a standard format.
+ */
 interface NormalizerInterface
 {
     /**
-     * Determine whether a given URL matches the conditions.
+     * Normalize a URI.
      *
-     * @param \Psr\Http\Message\UriInterface $uri
+     * @param \Psr\Http\Message\UriInterface $uri The URI to normalize
      *
-     * @return bool
+     * @return \Psr\Http\Message\UriInterface The normalized URI
      */
     public function normalize(UriInterface $uri);
 }
