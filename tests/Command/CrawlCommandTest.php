b5bc08578db7a4babec607df272bc838a2fad37d
Rob Bayliss <rob@lastcallmedia.com>
2016-09-16 09:20:15 -0400

    Switch to using mocks instead of concrete configuration in CrawlCommandTest

--- tests/Command/CrawlCommandTest.php
+++ tests/Command/CrawlCommandTest.php
@@ -3,31 +3,39 @@
 namespace LastCall\Crawler\Test;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use LastCall\Crawler\Command\CrawlCommand;
 use LastCall\Crawler\Configuration\Configuration;
+use LastCall\Crawler\Configuration\ConfigurationInterface;
 use LastCall\Crawler\Configuration\Loader\ConfigurationLoaderInterface;
 use LastCall\Crawler\CrawlerEvents;
 use LastCall\Crawler\Queue\ArrayRequestQueue;
+use Prophecy\Argument;
 use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class CrawlCommandTest extends \PHPUnit_Framework_TestCase
 {
     public function testRunsCrawler()
     {
-        $config = new Configuration('https://lastcallmedia.com');
-        $config['client'] = function () {
-            $handler = new MockHandler([new Response(200)]);
-
-            return new Client(['handler' => HandlerStack::create($handler)]);
-        };
-
-        $config['queue'] = function () {
-            return new ArrayRequestQueue([new Request('GET', 'https://lastcallmedia.com')]);
-        };
+        $started = $finished = 0;
+        $client = $this->prophesize(ClientInterface::class);
+        $config = $this->prophesize(ConfigurationInterface::class);
+        $config->getQueue()->willReturn(new ArrayRequestQueue());
+        $config->getClient()->willReturn($client);
+        $config->attachToDispatcher(Argument::type(EventDispatcherInterface::class))
+            ->will(function($args) use (&$started, &$finished) {
+                $args[0]->addListener(CrawlerEvents::START, function() use (&$started) {
+                    $started++;
+                });
+                $args[0]->addListener(CrawlerEvents::FINISH, function() use (&$finished) {
+                    $finished++;
+                });
+            });
 
         $loader = $this->prophesize(ConfigurationLoaderInterface::class);
         $loader->loadFile('crawler.php')->willReturn($config);
@@ -37,32 +45,29 @@ class CrawlCommandTest extends \PHPUnit_Framework_TestCase
         $tester = new CommandTester($command);
         $tester->execute([]);
 
-        $this->assertEquals(1, $config['queue']->count(ArrayRequestQueue::COMPLETE));
+        $this->assertEquals(1, $started);
+        $this->assertEquals(1, $finished);
     }
 
     public function testExecutesReset()
     {
-        $config = new Configuration('https://lastcallmedia.com');
-        $config['client'] = function () {
-            $handler = new MockHandler([new Response(200)]);
 
-            return new Client(['handler' => HandlerStack::create($handler)]);
-        };
-
-        $config['queue'] = function () {
-            return new ArrayRequestQueue([new Request('GET', 'https://lastcallmedia.com')]);
-        };
+        $client = $this->prophesize(ClientInterface::class);
+        $config = $this->prophesize(ConfigurationInterface::class);
+        $config->getClient()->willReturn($client->reveal());
+        $config->getQueue()->willReturn(new ArrayRequestQueue());
+        $config->attachToDispatcher(Argument::type(EventDispatcherInterface::class))
+            ->will(function($args) use (&$setup, &$teardown) {
+                $args[0]->addListener(CrawlerEvents::SETUP, function() use (&$setup) {
+                    $setup++;
+                });
+                $args[0]->addListener(CrawlerEvents::TEARDOWN, function() use (&$teardown) {
+                    $teardown++;
+                });
+            });
 
         $loader = $this->prophesize(ConfigurationLoaderInterface::class);
-        $loader->loadFile('crawler.php')->willReturn($config);
-
-        $setup = $teardown = 0;
-        $config->addListener(CrawlerEvents::SETUP, function () use (&$setup) {
-            ++$setup;
-        });
-        $config->addListener(CrawlerEvents::TEARDOWN, function () use (&$teardown) {
-            ++$teardown;
-        });
+        $loader->loadFile('crawler.php')->willReturn($config->reveal());
 
         $command = new CrawlCommand();
         $command->setLoader($loader->reveal());
